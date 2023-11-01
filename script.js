4da2eca0b0b2281689f411948a5f9339a716fd61
yongbioinfo <99473204+yongbioinfo@users.noreply.github.com>
2023-11-01 16:11:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const usTimeElement = document.getElementById('us-time');
+    const krUSTimeElement = document.getElementById('kr-us-time'); // 추가
     const toggleDarkModeButton = document.getElementById('toggle-darkmode');
 
     // 미국 주식 시장 시간을 가져오는 함수
     const fetchUSTime = () => {
-        const usTime = new Date().toLocaleTimeString('en-US', { timeZone: 'America/New_York' });
-        usTimeElement.textContent = usTime;
+        const usTime = new Date().toLocaleTimeString('en-US', { timeZone: 'America/New_York', hour12: false });
+        usTimeElement.textContent = "미국 시간: " + usTime;
+
+        const krUSTime = new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Seoul', hour12: false });
+        krUSTimeElement.textContent = "한국 시간: " + krUSTime;
+        
         setTimeout(fetchUSTime, 1000); // 1초마다 업데이트
     };
     fetchUSTime();
