41292b2ad403c3b1e53f4c1023c73aa5dea7278d
yongbioinfo <99473204+yongbioinfo@users.noreply.github.com>
2023-11-01 16:34:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
     const usTimeElement = document.getElementById('us-time');
-    const krUSTimeElement = document.getElementById('kr-us-time'); // 추가
+    const krUSTimeElement = document.getElementById('kr-us-time');
+    const chinaTimeElement = document.getElementById('china-time');
+    const japanTimeElement = document.getElementById('japan-time');
+    const ukTimeElement = document.getElementById('uk-time');
+    const indiaTimeElement = document.getElementById('india-time');
     const toggleDarkModeButton = document.getElementById('toggle-darkmode');
 
-    // 미국 주식 시장 시간을 가져오는 함수
-    const fetchUSTime = () => {
+    // 모든 시계를 업데이트하는 함수
+    const fetchAllTimes = () => {
+        // 미국 시간
         const usTime = new Date().toLocaleTimeString('en-US', { timeZone: 'America/New_York', hour12: false });
         usTimeElement.textContent = "미국 시간: " + usTime;
 
+        // 한국 시간
         const krUSTime = new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Seoul', hour12: false });
         krUSTimeElement.textContent = "한국 시간: " + krUSTime;
+
+        // 중국 시간
+        const chinaTime = new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Shanghai', hour12: false });
+        chinaTimeElement.textContent = "중국 시간: " + chinaTime;
+
+        // 일본 시간
+        const japanTime = new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Tokyo', hour12: false });
+        japanTimeElement.textContent = "일본 시간: " + japanTime;
+
+        // 영국 시간
+        const ukTime = new Date().toLocaleTimeString('en-US', { timeZone: 'Europe/London', hour12: false });
+        ukTimeElement.textContent = "영국 시간: " + ukTime;
+
+        // 인도 시간
+        const indiaTime = new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata', hour12: false });
+        indiaTimeElement.textContent = "인도 시간: " + indiaTime;
         
-        setTimeout(fetchUSTime, 1000); // 1초마다 업데이트
+        setTimeout(fetchAllTimes, 1000); // 1초마다 모든 시계 업데이트
     };
-    fetchUSTime();
+
+    fetchAllTimes();
 
     // 다크 모드 전환 함수
     let isDarkMode = false;
